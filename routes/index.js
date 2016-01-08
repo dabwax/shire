246dfe14389fbc02ba3c717fb2a25a72ce30aafe
dabwax <luizhrqas@gmail.com>
2016-01-08 09:50:10 -0200

    sign in/signup route init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/sign-in');
+});
+
+/* GET sign in. */
+router.get('/sign-in', function(req, res, next) {
+  res.render('index');
+});
+
+/* POST sign in. */
+router.post('/sign-in', function(req, res, next) {
+  res.render('sign_up');
+});
+
+/* GET sign up. */
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign_up');
+});
+
+/* POST sign up. */
+router.post('/sign-up', function(req, res, next) {
+  
+  res.render('sign_up');
 });
 
 module.exports = router;
